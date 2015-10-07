9584efadc2130fcb6d0bb0da43609d7fb464e3c8
Undo1 <parker@erwaysoftware.com>
2015-10-06 22:05:48 -0600

    Set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "flag_queues#index"
+
   resources :flag_queues
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
