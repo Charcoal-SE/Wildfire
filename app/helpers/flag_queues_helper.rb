b18d24ae1bb5e10528e196aa74401c93db5e3df9
Undo1 <parker@erwaysoftware.com>
2015-10-11 17:16:41 -0600

    Adjust flow

--- app/helpers/flag_queues_helper.rb
+++ app/helpers/flag_queues_helper.rb
@@ -5,19 +5,19 @@ module FlagQueuesHelper
     FlagQueue.all.each do |queue|
       next if queue.last_run + queue.frequency.seconds > DateTime.now
 
-      flags = queue.flags.where.not(:completed => true).load
-      puts flags
+      (1..queue.batch_size).each do
+        flags = queue.flags.where.not(:completed => true).load
+        puts flags
 
-      if flags.count > 0
-        (1..queue.batch_size).each do
-          queue.last_run = cycle_start
-          queue.save!
-
+        if flags.count > 0
           flag = flags.sort_by { |f| f.created_at }.first
           puts "Flagging #{flag.post_id} on #{flag.site.site_domain}"
           flag.send_flag
         end
       end
+
+      queue.last_run = cycle_start
+      queue.save!
     end
   end
 end
